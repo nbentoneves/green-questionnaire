e2c4f9d69bf7c4be4d3dc2538f349dc38eedef3f
Nuno Bento <nuno.bento@blip.pt>
2020-07-09 11:04:28 +0100

    Starting project

---
+++ src/main/java/com/exercise/green/questionnaire/GreenQuestionnaireApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.green.questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenQuestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenQuestionnaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/green/questionnaire/GreenQuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.green.questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenQuestionnaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
